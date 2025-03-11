fc8c609b1a644c0db1452f8e977085e7ef31c745
Fengzhanpeng156 <fengzhanpeng156@gmail.com>
2025-03-11 10:40:06 +0800

    lab2 work1 success

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -184,14 +184,12 @@ syscall(void)
             p->pid, p->name, num);
     p->trapframe->a0 = -1;
   }
-  
-
-  num = p->trapframe->a7;
-  if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    p->trapframe->a0 = syscalls[num]();
-  } else {
-    printf("%d %s: unknown sys call %d\n",
-            p->pid, p->name, num);
-    p->trapframe->a0 = -1;
-  }
+//   num = p->trapframe->a7;
+//   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+//     p->trapframe->a0 = syscalls[num]();
+//   } else {
+//     printf("%d %s: unknown sys call %d\n",
+//             p->pid, p->name, num);
+//     p->trapframe->a0 = -1;
+//   }
 }
