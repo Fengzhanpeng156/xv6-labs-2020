ba14a7d9ed262b87f596247e629079745fb022fb
Fengzhanpeng156 <fengzhanpeng156@gmail.com>
2025-03-20 16:25:17 +0800

    lab3 work1 firsttest

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+int
+pgtblprint(pagetable_t pagetable, int depth){
+    for(int i = 0; i < 512; i++){
+        pte_t pte = pagetable[i];
+
+        if (pte & PTE_V)  //如果页表有效
+        {   //按格式打印页表
+            printf("..");
+            for(int j = 0; j <depth; ++j){
+                printf(" ..");
+            }
+            printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+            //如果不是叶节点，递归打印子节点
+            //只设置了 PTE_V，没有 PTE_R、PTE_W、PTE_X，表示该 PTE 存储的是 子页表的物理地址。
+            if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+                uint64 child = PTE2PA(pte);
+                pgtblprint((pagetable_t)child, depth + 1);
+            }
+        }     
+    }
+    return 0;
+}
+
+int
+vmprint(pagetable_t pagetable){
+    printf("page table %p\n", pagetable);
+    return pgtblprint(pagetable, 0);
+}
